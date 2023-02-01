6c14b023bf20d90a2d0d213b39795cc2f109cc6c
IzbassarO <124159460+IzbassarO@users.noreply.github.com>
2023-02-01 18:31:27 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Diyar");
-        System.out.println("Dias and Izbassar");
+        System.out.println("Izbassar");
+        System.out.println("Dias and Diyar");
     }
 }
